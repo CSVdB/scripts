baaba9eee5e99150189f1bdfbe66e12056fc8c1b
Nickske666 <nick.van.den.broeck@hotmail.com>
2017-08-25 15:59:02 -0400

    First commit

---
+++ sync-wf.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+sync () {
+  name=$1
+  sep=$2
+  runGit () {
+    if [[ "$name" == "" ]]
+    then
+        git "$@"
+    else
+        $name git $1 $sep "${@:2}"
+    fi
+  }
+
+  runGit fetch origin
+  runGit status
+  runGit merge --ff-only origin/master
+  changes=$(runGit status --porcelain)
+  if [[ "$changes" != "" ]]
+  then
+    runGit add .
+    runGit commit -m "Sync commit from $(uname --nodename)"
+  fi
+  runGit push
+}
+
+sync /home/nick/.local/bin/wolf "--"
+sync pass
+cd /home/nick/workflow
+sync 
+cd /home/nick/ref/sus-depot
+sync 
+cd /home/nick/scripts
+sync 
