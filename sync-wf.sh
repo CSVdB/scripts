bc854e33d4e0c2dab04265dafec4831af0d5064d
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-01-25 19:37:00 +0100

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- sync-wf.sh
+++ sync-wf.sh
@@ -20,18 +20,22 @@ sync () {
   git push --all --repo=origin
 }
 
-sync ~/workflow
-sync ~/ref/books
-sync ~/ref/sus-depot/shared/scripts
-sync ~/ref/sus-depot
-sync ~/ref/personal_growth
-sync ~/ref/body/food
-sync ~/ref/travel
-sync ~/ref/body/fitness
-# sync ~/ref/talks
-# sync ~/ref/CV
-# sync ~/ref/swe/jobhunt
-# sync ~/ref/company
-# sync ~/ref/big-company
-sync ~/ref/administration
-intray sync
+sync ~/workflow;
+sync ~/ref/books;
+sync ~/ref/sus-depot/shared/scripts;
+sync ~/ref/sus-depot;
+sync ~/ref/personal-growth;
+sync ~/ref/body/food;
+sync ~/ref/travel;
+sync ~/ref/body/fitness;
+sync ~/ref/relu;
+sync ~/ref/talks;
+sync ~/ref/CV;
+# sync ~/ref/swe/jobhunt;
+# sync ~/ref/company;
+# sync ~/ref/big-company;
+sync ~/ref/administration;
+sync ~/.scripts;
+intray sync;
+rm -rf ~/snap;
+rm -rf ~/NoMachine;
