4f128e4055968e16daa2728c4a97907cf4bca781
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-04-01 10:38:34 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- install-user.sh
+++ install-user.sh
@@ -1,12 +1,13 @@
 sudo apt-get install git
 sudo apt-get install vim
-sudo apt-get install haskell-stack
 sudo apt-get install rxvt-unicode
 sudo apt-get install zsh
 sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt # change default terminal to urxvt
 chsh -s $(which zsh) # Change default shell to zsh. Only applies once desktop env is restarted.
 
+
 # Intray
+wget -qO- https://get.haskellstack.org/ | sh
 stack install autoexporter
 git clone https://github.com/NorfairKing/intray.git --recursive
 cd intray
