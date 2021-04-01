7f13e46de86ef1084ab191be56f20d5d5af652af
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-04-01 11:17:34 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- create-ssh-key.sh
+++ create-ssh-key.sh
@@ -1,6 +1,7 @@
 ssh-keygen -t ed25519 -C "vandenbroeck@cs-vdb.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
+sudo apt-get install xclip
 xclip -selection clipboard < ~/.ssh/id_ed25519.pub
 
 
