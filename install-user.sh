2f462110f28cf5717f2b68ae109e400ee894bd5e
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-04-01 09:37:24 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- install-user.sh
+++ install-user.sh
@@ -1,3 +1,7 @@
 sudo apt-get install git
 sudo apt-get install vim
+sudo apt-get install haskell-stack
+sudo apt-get install rxvt-unicode
+sudo apt-get install zsh
+sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
 
