fd848de2cb344bd72dc759ffadccaa965d4b9ff6
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-04-01 09:54:32 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- install-user.sh
+++ install-user.sh
@@ -3,5 +3,6 @@ sudo apt-get install vim
 sudo apt-get install haskell-stack
 sudo apt-get install rxvt-unicode
 sudo apt-get install zsh
-sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
+sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt # change default terminal to urxvt
+chsh -s $(which zsh) # Change default shell to zsh. Only applies once desktop env is restarted.
 
