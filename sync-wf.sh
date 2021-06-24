4014c886d4a1b7af0216c709ea0039887746a56e
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-06-24 06:14:57 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-Gen-9

--- sync-wf.sh
+++ sync-wf.sh
@@ -40,7 +40,6 @@ sync /home/nick/ref/company;
 sync /home/nick/ref/administration;
 sync /home/nick/ref/network;
 sync /home/nick/ref/dsl;
-sync /home/nick/ref/pact;
 sync /home/nick/ref/strategy-consulting;
 sync /home/nick/ref/engineering;
 sync /home/nick/.scripts;
