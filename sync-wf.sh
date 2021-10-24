42b41e7cf42b2ba6a1e2919aff0825ca61b466ef
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-10-24 04:23:13 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- sync-wf.sh
+++ sync-wf.sh
@@ -24,7 +24,6 @@ rm -rf /home/nick/snap;
 sync /home/nick/workflow;
 sync /home/nick/ref/personal-growth;
 sync /home/nick/ref/platonic/personal;
-# sync /home/nick/ref/platonic;
 sync /home/nick/ref/books;
 sync /home/nick/.scripts;
 sync /home/nick/ref/sus-depot;
