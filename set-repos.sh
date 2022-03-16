38d97e3c46ee7a9c2e48b9266870153341f6bc09
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2022-03-16 07:15:28 +0100

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- set-repos.sh
+++ set-repos.sh
@@ -5,8 +5,20 @@ set -e
 setRepo () {
   name=$1
   directory=$2
-  mkdir -p ~/$directory
-  git clone git@github.com:CSVdB/$name.git ~/$directory
+  mkdir -p ~/tmp/$directory
+  git clone git@github.com:CSVdB/$name.git ~/tmp/$directory
 }
 
-setRepo my_workflow tmp/workflow
+setRepo my_workflow workflow
+setRepo personal-growth personal-growth
+setRepo ref/platonic/personal
+setRepo ref/books
+setRepo .scripts
+setRepo ref/entrepreneur
+setRepo ref/body
+setRepo ref/travel
+setRepo ref/CV
+setRepo ref/jobhunt
+setRepo ref/company
+setRepo ref/administration
+setRepo ref/network
--- sync-wf.sh
+++ sync-wf.sh
@@ -20,22 +20,20 @@ sync () {
   git push --all --repo=origin
 }
 
-rm -rf /home/nick/snap;
-sync /home/nick/workflow;
-sync /home/nick/ref/personal-growth;
-sync /home/nick/ref/platonic/personal;
-sync /home/nick/ref/books;
-sync /home/nick/.scripts;
-sync /home/nick/ref/entrepreneur;
-sync /home/nick/ref/body;
-sync /home/nick/ref/travel;
-sync /home/nick/ref/CV;
-sync /home/nick/ref/jobhunt;
-sync /home/nick/ref/company;
-sync /home/nick/ref/administration;
-sync /home/nick/ref/network;
-sync /home/nick/ref/past/strategy-consulting;
-intray sync;
+sync /home/nick/workflow
+sync /home/nick/ref/personal-growth
+sync /home/nick/ref/platonic/personal
+sync /home/nick/ref/books
+sync /home/nick/.scripts
+sync /home/nick/ref/entrepreneur
+sync /home/nick/ref/body
+sync /home/nick/ref/travel
+sync /home/nick/ref/CV
+sync /home/nick/ref/jobhunt
+sync /home/nick/ref/company
+sync /home/nick/ref/administration
+sync /home/nick/ref/network
+intray sync
 python3 /home/nick/Pictures/background/walltext.py &
 
 # redshift &
