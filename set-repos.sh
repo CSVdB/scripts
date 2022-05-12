8fda60ed13fd1139d71cad0862f94b198e064aa6
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2022-05-12 10:33:25 +0200

    Sync commit from nick-ThinkPad-X1-Carbon-6th

--- set-repos.sh
+++ set-repos.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+repo=~/tmp
+
 setRepo () {
   name=$1
   directory=$2
-  mkdir -p ~/tmp/$directory
-  git clone git@github.com:CSVdB/$name.git ~/tmp/$directory --recursive
+  mkdir -p $repo/$directory
+  git clone git@github.com:CSVdB/$name.git $repo/$directory --recursive
 }
 
 setRepo my_workflow workflow
@@ -18,11 +20,9 @@ setRepo entrepreneurship ref/entrepreneur
 setRepo body ref/body
 setRepo travel ref/travel
 setRepo CV ref/CV
-setRepo jobhunt ref/jobhunt
-setRepo company ref/company
 setRepo administration ref/administration
 setRepo network ref/network
 setRepo PACT-deployment ref/code/deployment
 setRepo PACT-personal ref/pact
 setRepo platonic ref/platonic
-git clone git@github.com:NorfairKing/salsa.git /ref/code/salsa
+git clone git@github.com:NorfairKing/salsa.git $repo/ref/code/salsa
