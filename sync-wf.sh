9b84f5a1c78f21669f7e1e99e998b73d0b9edacf
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2023-03-10 14:00:47 +0100

    Sync commit from nick-ThinkPad-X1-Extreme-Gen-4i

--- sync-wf.sh
+++ sync-wf.sh
@@ -26,6 +26,7 @@ sync /home/nick/ref/personal-growth
 sync /home/nick/ref/platonic/personal
 sync /home/nick/ref/platonic/companies/devils
 sync /home/nick/ref/platonic/companies/betabay
+sync /home/nick/ref/platonic/companies/sisera
 sync /home/nick/ref/books
 sync /home/nick/.scripts
 sync /home/nick/.install
